2a9cdaee27a9696f226c414579296f224a46fc47
Alina Vanieva <vanieva.alina@yandex.ru>
2018-06-07 17:59:28 +0300

    adds insertion sort

---
+++ algorithms/insertionSort.js
@@ -0,0 +1,22 @@
+// Сортировка вставкой / Insertion Sort
+
+const insertionSort = arr => {
+
+    for (let i = 1, l = arr.length; i < l; i++) {
+        const current = arr[i];
+        let j = i;
+
+        while (j > 0 && arr[j - 1] > current) {
+            arr[j] = arr[j - 1];
+            j--;
+        }
+        arr[j] = current;
+    }
+
+    return arr;
+};
+
+console.log(
+    'insertionSort([5, 8, 52, 15, 1, 3, 4, 9, 60, 63, 72])',
+    insertionSort([5, 8, 52, 15, 1, 3, 4, 9, 60, 63, 72])
+);

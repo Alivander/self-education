08989516d6665e5177a3c3e37036ca0e8b5caf1a
Alina Vanieva <vanieva.alina@yandex.ru>
2018-06-07 18:17:10 +0300

    adds variant  insertion sort

--- algorithms/insertionSort.js
+++ algorithms/insertionSort.js
@@ -1,22 +1,40 @@
 // Сортировка вставкой / Insertion Sort
 
-const insertionSort = arr => {
+// классический вариант, ES5
+
+function insertionSort(arr) {
 
     for (let i = 1, l = arr.length; i < l; i++) {
-        const current = arr[i];
-        let j = i;
+        var current = arr[i];
+        var j = i;
 
         while (j > 0 && arr[j - 1] > current) {
             arr[j] = arr[j - 1];
             j--;
         }
+
         arr[j] = current;
     }
 
     return arr;
 };
 
+// без дополнительной переменной, ES6
+
+const insertionSortV2 = arr => {
+
+    for (let i = 1, l = arr.length; i < l; i++) {
+
+        for (let j = i; j > 0 && arr[j - 1] > arr[j]; j--) {
+            [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
+        }
+
+    }
+
+    return arr;
+};
+
 console.log(
-    'insertionSort([5, 8, 52, 15, 1, 3, 4, 9, 60, 63, 72])',
-    insertionSort([5, 8, 52, 15, 1, 3, 4, 9, 60, 63, 72])
+    'insertionSortV2([5, 8, 52, 15, 1, 3, 4, 9, 60, 63, 72])',
+    insertionSortV2([5, 8, 52, 15, 1, 3, 4, 9, 60, 63, 72])
 );

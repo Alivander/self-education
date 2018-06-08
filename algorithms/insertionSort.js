0e6f211a0b2639a6e181a733f75ca4b838c9235f
Alina Vanieva <vanieva.alina@yandex.ru>
2018-06-08 11:49:23 +0300

    update insertion sort

--- algorithms/insertionSort.js
+++ algorithms/insertionSort.js
@@ -1,12 +1,11 @@
 // Сортировка вставкой / Insertion Sort
 
-// классический вариант, ES5
 
-function insertionSort(arr) {
+const insertionSort = arr => {
 
     for (let i = 1, l = arr.length; i < l; i++) {
-        var current = arr[i];
-        var j = i;
+        const current = arr[i];
+        let j = i;
 
         while (j > 0 && arr[j - 1] > current) {
             arr[j] = arr[j - 1];
@@ -19,22 +18,7 @@ function insertionSort(arr) {
     return arr;
 };
 
-// без дополнительной переменной, ES6
-
-const insertionSortV2 = arr => {
-
-    for (let i = 1, l = arr.length; i < l; i++) {
-
-        for (let j = i; j > 0 && arr[j - 1] > arr[j]; j--) {
-            [arr[j], arr[j - 1]] = [arr[j - 1], arr[j]];
-        }
-
-    }
-
-    return arr;
-};
-
 console.log(
-    'insertionSortV2([5, 8, 52, 15, 1, 3, 4, 9, 60, 63, 72])',
-    insertionSortV2([5, 8, 52, 15, 1, 3, 4, 9, 60, 63, 72])
+    'insertionSort([5, 8, 52, 15, 1, 3, 4, 9, 60, 63, 72])',
+    insertionSort([5, 8, 52, 15, 1, 3, 4, 9, 60, 63, 72])
 );

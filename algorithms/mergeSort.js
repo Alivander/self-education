d507cf2b7498369b0fdec6e97b18372f2b58532b
Alina Vanieva <vanieva.alina@yandex.ru>
2018-06-11 16:37:03 +0300

    adds merge sort

---
+++ algorithms/mergeSort.js
@@ -0,0 +1,45 @@
+// Сортировка слиянием / Merge sort
+
+// Вспомогательная функция,
+// сливает два массива в порядке возрастания элементов
+const merge = (arrFirst, arrSecond) => {
+    const arrSort = [];
+    let i = j = 0;
+
+    while (i < arrFirst.length && j < arrSecond.length) {
+        arrSort.push(
+            (arrFirst[i] < arrSecond[j]) ? arrFirst[i++] : arrSecond[j++]
+        );
+    }
+
+    // обрабатываем последний элемент при разной длине массивов
+    // и возвращаем один отсортированный массив
+    return [...arrSort, ...arrFirst.slice(i), ...arrSecond.slice(j)];
+};
+
+const mergeSort = arr => {
+
+    // Проверяем корректность переданных данных
+    if (!arr || !arr.length) {
+        return null;
+    }
+
+    //Если массив содержит один элемент просто возвращаем его
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    // Находим середину массива и делим его на два
+    const middle = Math.floor(arr.length / 2);
+    const arrLeft = arr.slice(0, middle);
+    const arrRight = arr.slice(middle);
+
+    // Для новых массивов снова вызываем сортировку,
+    // сливаем их и возвращаем снова единый массив
+    return merge(mergeSort(arrLeft), mergeSort(arrRight));;
+};
+
+console.log(
+    'mergeSort([5, 1, 8, 9, 4, 6, 2, 3, 7, 0]',
+    mergeSort([5, 1, 8, 9, 4, 6, 2, 3, 7, 0])
+);

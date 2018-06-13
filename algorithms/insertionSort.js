644f4261d3f172e412a0a0c4c2018cceef974de4
Alina Vanieva <vanieva.alina@yandex.ru>
2018-06-13 03:14:16 +0300

    adds Shell sort

--- algorithms/insertionSort.js
+++ algorithms/insertionSort.js
@@ -1,6 +1,5 @@
 // Сортировка вставкой / Insertion Sort
 
-
 const insertionSort = arr => {
 
     for (let i = 1, l = arr.length; i < l; i++) {
---
+++ algorithms/shellSort.js
@@ -0,0 +1,30 @@
+// Сортировка Шелла / Shell sort
+
+const ShellSort = arr => {
+    const l = arr.length;
+    let gap = Math.floor(l / 2);
+
+    while (gap >= 1) {
+
+        for (let i = gap; i < l; i++) {
+            const current = arr[i];
+            let j = i;
+
+            while (j > 0 && arr[j - gap] > current) {
+                arr[j] = arr[j - gap];
+                j -= gap;
+            }
+
+            arr[j] = current;
+        }
+
+        gap = Math.floor(gap / 2);
+    }
+
+    return arr;
+};
+
+console.log(
+    'ShellSort([5, 8, 52, 15, 1, 3, 4, 9, 60, 63, 72])',
+    ShellSort([5, 8, 52, 15, 1, 3, 4, 9, 60, 63, 72])
+);
